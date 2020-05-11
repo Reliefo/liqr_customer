bd5d5ebc960a090a12bce8522c26bd1aa597bbda
Akash VijayKumar Jandial <akash.jandial.tcs@cl191312.hk.intraxa>
2020-05-11 17:04:34 +0800

    Adding new login feature

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import {
-  FormGroup,
-  FormControl,
-} from "react-bootstrap";
+import { FormGroup, FormControl } from "react-bootstrap";
+import ReactDOM from "react-dom";
 import LoaderButton from "../components/LoaderButton";
+import axios from "axios";
+import AppWrapper from "../App";
 import { Auth } from "aws-amplify";
 
 export default class Signup extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       isLoading: false,
       email: "",
+      name: "",
       password: "",
       confirmPassword: "",
       confirmationCode: "",
@@ -36,44 +36,51 @@ export default class Signup extends Component {
     this.setState({
       [event.target.id]: event.target.value
     });
-  }
+  };
 
   handleSubmit = async event => {
     event.preventDefault();
-  
     this.setState({ isLoading: true });
-  
-    try {
-      const newUser = await Auth.signUp({
-        username: this.state.email,
-        password: this.state.password
-      });
-      this.setState({
-        newUser
-      });
-    } catch (e) {
-      alert(e.message);
-    }
-  
+
+    let bodyFormData = new FormData();
+    bodyFormData.set("unique_id", localStorage.getItem("uniqueId"));
+    bodyFormData.set("password", this.state.password);
+    bodyFormData.set("email_id", this.state.email);
+    bodyFormData.set("name", this.state.name);
+    bodyFormData.set("table_id", localStorage.getItem("table_id"));
+
+    axios({
+      method: "post",
+      url: "https://liqr.cc/user_register",
+      data: bodyFormData
+    }).then(response => {
+      const { data } = response;
+      localStorage.setItem("jwt", data.jwt);
+      localStorage.setItem("refreshToken", data.refresh_token);
+      localStorage.setItem("user_id", data.user_id);
+      localStorage.setItem("name", data.name);
+      ReactDOM.render(<AppWrapper />, document.getElementById("root"));
+      this.props.history.push("/");
+    });
     this.setState({ isLoading: false });
-  }
-  
+  };
+
   handleConfirmationSubmit = async event => {
     event.preventDefault();
-  
+
     this.setState({ isLoading: true });
-  
+
     try {
       await Auth.confirmSignUp(this.state.email, this.state.confirmationCode);
       await Auth.signIn(this.state.email, this.state.password);
-  
+
       this.props.userHasAuthenticated(true);
       this.props.history.push("/");
     } catch (e) {
       alert(e.message);
       this.setState({ isLoading: false });
     }
-  }
+  };
 
   renderConfirmationForm() {
     return (
@@ -127,6 +134,14 @@ export default class Signup extends Component {
             value={this.state.confirmPassword}
             onChange={this.handleChange}
             type="password"
+          />
+        </FormGroup>
+        <FormGroup controlId="name" bsSize="large">
+          <label>Name</label>
+          <FormControl
+            value={this.state.name}
+            onChange={this.handleChange}
+            type="name"
           />
         </FormGroup>
         <LoaderButton
