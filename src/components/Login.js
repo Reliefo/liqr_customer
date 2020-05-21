6c4b4df71cb2f9aac8b061612fe9fcda76a2ea0d
Akash VijayKumar Jandial <akash.jandial.tcs@Akash-Jandials-Macbook.local>
2020-05-21 10:47:18 +0800

    Adding new layout for Sign In Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,10 +44,16 @@ export default class Login extends Component {
   render() {
     return (
       <div className="Login">
+        <div className="sign-in">Sign In</div>
         <form onSubmit={this.handleSubmit}>
           <FormGroup controlId="email" bsSize="large">
-            <label>Email</label>
+            <label className="sign-in-label">Email ID</label>
             <FormControl
+              style={{
+                fontSize: "15px",
+                fontFamily: "Poppins"
+              }}
+              placeholder="sample@emailid.com"
               autoFocus
               type="email"
               value={this.state.email}
@@ -55,8 +61,13 @@ export default class Login extends Component {
             />
           </FormGroup>
           <FormGroup controlId="password" bsSize="large">
-            <label>Password</label>
+            <label className="sign-in-label">Password</label>
             <FormControl
+              style={{
+                fontSize: "15px",
+                fontFamily: "Poppins"
+              }}
+              placeholder="********"
               value={this.state.password}
               onChange={this.handleChange}
               type="password"
@@ -69,14 +80,55 @@ export default class Login extends Component {
             type="submit"
             isLoading={this.state.isLoading}
             text="Login"
+            className="sign-in-button"
             loadingText="Logging in…"
           />
         </form>
-        <div className="signup-button">
-          <a href="/register">
-            SignUp
-          </a>
+        <div className="sign-in-or">or</div>
+        <div>
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Google"
+            style={{
+              marginRight: "10%",
+              float: "left",
+              width: "45%",
+            }}
+            className="sign-in-google"
+            loadingText="Logging in…"
+          />
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            style={{
+              width: "45%"
+            }}
+            isLoading={this.state.isLoading}
+            text="Facebook"
+            className="sign-in-facebook"
+            loadingText="Logging in…"
+          />
         </div>
+        <div className="sign-in-member">
+          Not a member yet ? <span style= {{ color: '#ffb023' }}> Sign Up</span>
+        </div>
+        <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            style={{marginTop: '5%'}}
+            isLoading={this.state.isLoading}
+            text="Skip Sign In"
+            className="sign-in-button"
+            loadingText="Logging in…"
+          />
       </div>
     );
   }
