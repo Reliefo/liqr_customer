6bcc024788fc0bbc7691561e9d56ead2ad94f747
Akash VijayKumar Jandial <akash.jandial.tcs@Akash-Jandials-Macbook.local>
2020-06-03 14:09:13 +0800

    Fixing navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -90,10 +90,17 @@ const Navbar = props => {
         )}
       </div>
       <Menu disableAutoFocus width={"55%"}>
-        <a className="menu-item shadow-menu" href="/login">
-          <img className="navbar-menu-icon" src={login} alt="Login" /> Login/
-          SignUp
-        </a>
+        {localStorage.getItem("registeredUser") === "false" ? (
+          <a className="menu-item shadow-menu" href="/login">
+            <img className="navbar-menu-icon" src={login} alt="Login" /> Login/
+            SignUp
+          </a>
+        ) : (
+          <a className="menu-item shadow-menu" href="/">
+            <img className="navbar-menu-icon" src={login} alt="Login" /> Logged
+            In : {localStorage.getItem("name")}
+          </a>
+        )}
         <hr />
 
         <a className="menu-item" href="/menu">
